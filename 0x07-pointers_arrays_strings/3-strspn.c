48013c6c408098e6d620926bea8c622d81d7b36d
robel16 <99750963+robel16@users.noreply.github.com>
2022-03-28 13:04:27 -0600

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,25 +1,28 @@
 #include "main.h"
-
 /**
- * _strspn - gets lengthof a prefix substring
- * @s: string to check
- * @accept: string to check against
- *
- * Return: number of bytes of s in accept
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j; 
+	unsigned int n = 0;
+	int r;
 
-	for (i = 0; s[i]; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		for (r = 0; accept[r]; r++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[r])
+			{
+				n++;
 				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
-		if (!accept[j])
-			break;
+		s++;
 	}
-	return (i);
+	return (n);
 }
